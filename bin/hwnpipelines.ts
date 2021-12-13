8f5b7046600b8e3da61426e6492f5a69e4324486
Julio Cesar Arbieto <arbieto.jc@gmail.com>
2021-12-13 11:14:40 -0500

    add new deploy in account jarbieto

--- bin/hwnpipelines.ts
+++ bin/hwnpipelines.ts
@@ -6,7 +6,7 @@ import { HWNPipelineStack } from '../lib/hwnpipelines-stack';
 const app = new cdk.App();
 new HWNPipelineStack(app, 'HwnpipelinesStack', {
   env: {
-    account: '978928857807',
+    account: '561215544381',
     region: 'us-east-1',
   }
 });
