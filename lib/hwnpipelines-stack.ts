3e58337dd0261264427bd338d613aa22a127a059
Julio Cesar Arbieto <arbieto.jc@gmail.com>
2021-12-13 14:16:31 -0500

    deploy stack bootstraps

--- lib/hwnpipelines-stack.ts
+++ lib/hwnpipelines-stack.ts
@@ -8,7 +8,7 @@ export class HWNPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'HWNPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('deekob/hwnpipeline', 'main'),
+        input: CodePipelineSource.gitHub('JulioCesarArbieto/hwnpipeline', 'main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
